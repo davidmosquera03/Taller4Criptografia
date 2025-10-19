25d190604a2d6478b0fc8fdbc23fa40be7402fc1
David Hernandez <davidmosquera@uninorte.edu.co>
2025-10-19 16:33:43 -0500

    started T3

--- rsa.py
+++ rsa.py
@@ -100,3 +100,47 @@ class ModifiedRSA:
         xpp = (qinv*(xp-xq))%p
         x = xq+xpp*q
         return x
+    
+    def CorruptIrsa_a(self, sk, y):
+        """
+        Corrupcion de resultado intermedio
+        Invierte bit de xp
+        """
+        (p, q, dp, dq, qinv) = sk
+        xp = pow(y,dp,p)
+
+        # Cambiar bit
+        xp_corrupto = xp ^ (1 << 0)
+        xq = pow(y,dq,q)
+
+        xpp = (qinv*(xp_corrupto-xq))%p
+        x_hat = xq+xpp*q
+        return x_hat
+    
+    def CorruptIrsa_b(self, sk, y):
+        """
+        Corrupcion de entrada
+        Invierte bit de y
+        """
+        y_corrupto = y ^ (1 << 0)
+        (p, q, dp, dq, qinv) = sk
+        xp = pow(y_corrupto,dp,p)
+        xq = pow(y_corrupto,dq,q)
+
+         # aplicacion CRT
+        xpp = (qinv*(xp-xq))%p
+        x_hat = xq+xpp*q
+        return x_hat
+    
+    def CorruptIrsa_c(self, sk, y):
+        """
+        Corrupcion de exponente
+        """
+        return
+    
+    def CorruptIrsa_d(self, sk, y):
+        """
+        Corrupcion de qinv
+        """
+        return
+    
